1a43db9ab7fa046f84b34e391e2cf77f79ee432b
santiagoibarzabal <santiagoibarzabal91@gmail.com>
2024-12-27 01:05:57 +0100

    fix: remove unused factories

--- app/TaskEntries/Infrastructure/Repositories/Models/TaskEntry.php
+++ app/TaskEntries/Infrastructure/Repositories/Models/TaskEntry.php
@@ -2,15 +2,10 @@
 
 namespace App\TaskEntries\Infrastructure\Repositories\Models;
 
-use Database\Factories\ProjectFactory;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TaskEntry extends Model
 {
-    /** @use HasFactory<ProjectFactory> */
-    use HasFactory;
-
     /**
      * The attributes that are mass assignable.
      *
--- app/Tasks/Infrastructure/Repositories/Models/Task.php
+++ app/Tasks/Infrastructure/Repositories/Models/Task.php
@@ -2,15 +2,10 @@
 
 namespace App\Tasks\Infrastructure\Repositories\Models;
 
-use Database\Factories\ProjectFactory;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    /** @use HasFactory<ProjectFactory> */
-    use HasFactory;
-
     /**
      * The attributes that are mass assignable.
      *
